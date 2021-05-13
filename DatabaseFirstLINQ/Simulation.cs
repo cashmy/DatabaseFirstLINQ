72c356d171aa2389bd13d90fc30613fe86ef89c7
cashmy <cmyers880@gmail.com>
2021-05-13 15:46:00 -0500

    Added Menu simulation and generic User Interface class.

---
+++ DatabaseFirstLINQ/Simulation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseFirstLINQ
+{
+    public class Simulation
+    {
+
+        public void RunSimulation()
+        {
+            bool Check = UserInterface.DisplayWelcome();
+            if (Check)
+            {
+                SimulationMenu()
+            }
+            else
+            {
+                // return - just exit
+            }
+        }
+
+        public void SimulationMenu()
+        {
+            bool WillProceed = true;
+            while (WillProceed)
+            {
+                int UserOption = UserInterface.SimulationMainMenu()
+
+                if (UserOption == 1)
+                {
+                    // Execute Menu Option 1
+                }
+                else if (UserOption == 2)
+                {
+                    // Execute Menu Option 2
+                }
+                else if (UserOption == 3)
+                {
+                    // Execute Menu Option 3
+                }
+                else if (UserOption == 4)
+                {
+                    // Execute Menu Option 4
+                }
+                else if (UserOption == 5)
+                {
+                    //UserInterface.OutputText('Goodbye.')
+                    // return - just exit
+                }
+                else
+                {
+                    WillProceed = false;
+                }
+            }
+        }
+    }
+
+}
---
+++ DatabaseFirstLINQ/UserInterface.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseFirstLINQ
+{
+    public class UserInterface
+    {
+
+        public static int SimulationMainMenu()
+        {
+            bool ValidateUserSelectionStatus = false;
+            int ValidateUserSelectionOption = 0;
+
+            while (!ValidateUserSelectionStatus)
+            {
+                Console.WriteLine("\n\t\t-Shopping Cart Processing-");
+                Console.WriteLine("\tPress -1- to List all products");
+                Console.WriteLine("\tPress -2- to Add a product to your cart");
+                Console.WriteLine("\tPress -3- to View your shopping cart");
+                Console.WriteLine("\tPress -4- to Remove a product from your cart");
+                Console.WriteLine("\tPress -5- to Exit");
+
+                int UserInput = Console.Read();
+
+                ValidateUserSelectionStatus = ValidateMainMenu(UserInput);
+                if (ValidateUserSelectionStatus) { ValidateUserSelectionOption = UserInput};
+            }
+
+            return ValidateUserSelectionOption;
+
+        }
+
+        public bool ValidateMainMenu(int UserInput)
+        {
+            if (UserInput > 0 && UserInput < 6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool DisplayWelcome()
+        {
+            Console.Write("\nWelcome to the Shopping Cart manager. \n");
+
+            bool UserResponse = BoolPrompt("Would you like to order products today? (y/n): ")
+            if (UserResponse)
+                { return true; }
+            else
+            {
+                Console.WriteLine("Please sign off and conserve your precious technological resources.");
+                return false;
+            }
+        }
+
+        public void OutputText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public bool BoolPrompt(string text)
+        {
+            Console.WriteLine(text);
+            string UserInput = Console.ReadLine().ToLower();
+
+            switch (UserInput)
+            {
+                case "y":
+                    {
+                        return true;
+                    }
+                case "yes":
+                    {
+                        return true;
+                    }
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+    }
+}
